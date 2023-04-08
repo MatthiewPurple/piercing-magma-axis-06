2f0093e534fabe2540c0133bd5327917f6a24372
Matthiew Purple <matthiewpurple@hotmail.com>
2023-04-08 16:41:25 +0200

    no-repel

--- PiercingMagmaAxis06.cs
+++ PiercingMagmaAxis06.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 using Il2Cpp;
 using piercing_magma_axis_06;
 
-[assembly: MelonInfo(typeof(PiercingMagmaAxis06), "Piercing Magma Axis [including Repel] (ver. 0.6)", "1.0.0", "Matthiew Purple")]
+[assembly: MelonInfo(typeof(PiercingMagmaAxis06), "Piercing Magma Axis [no Repel] (ver. 0.6)", "1.0.0", "Matthiew Purple")]
 [assembly: MelonGame("アトラス", "smt3hd")]
 
 namespace piercing_magma_axis_06;
@@ -16,7 +16,7 @@ public class PiercingMagmaAxis06 : MelonMod
         public static void Postfix(ref int nskill, ref uint __result)
         {
             // If the attack is Magma Axis and it's resisted/blocked/drained/repelled
-            if (nskill == 161 && (__result < 100 || (__result > 999 && __result < 1000000000)))
+            if (nskill == 161 && (__result < 100 || (__result > 999 && __result < 100000) || (__result > 200000 && __result < 1000000000)))
             {
                 __result = 100; // Forces the affinity to become "neutral"
                 nbMainProcess.nbGetMainProcessData().d31_kantuu = 1; // Displays the "Pierced!" message
@@ -30,7 +30,7 @@ public class PiercingMagmaAxis06 : MelonMod
     {
         public static void Postfix(ref int id, ref string __result)
         {
-            if (id == 161) __result = "Fire damage to one foe. \nIgnores all resistances."; // New skill description of Magma Axis
+            if (id == 161) __result = "Fire damage to one foe. \nIgnores all resistances \nexcept Repel."; // New skill description of Magma Axis
         }
     }
 }
